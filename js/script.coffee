f3f07b509af885f28276121ec4f9a35522d96566
Noah Singer <noah.singer@gmail.com>
2011-04-22 21:21:15 -0500

    added display of errors at top of form

--- js/script.coffee
+++ js/script.coffee
@@ -1,10 +1,32 @@
-$(document).ready ->
+create_errors_div = ->
+	if $("#form_errors").size( ) < 1
+		$("<div id=\"form_errors\"><h1>The following errors kept the form from being submitted</h1><ul></ul></div>").prependTo( "form" )
+		
+remove_errors_div = ->
+	$("#form_errors").remove( )
+		
+show_error = ( message ) ->
+	$("<li>#{message}</li>").appendTo( "#form_errors ul" )
+
+clear_errors = ->
+	if $("#form_errors").size( ) > 0
+		$("#form_errors ul").empty( )
+
+$(document).ready ->			
 	$("form").submit ->
+		errors = []
 		$("form input[type=text]").each ->
 			if $(this).attr( "value" ) is ""
 				$(this).addClass "in_error"
 				$("label[for=" + $(this).attr( "id" ) + "]").addClass "in_error"
+				errors.push "#{$("label[for=" + $(this).attr( "id" ) + "]").html( )} cannot be blank"
 				$(this).focus ->
 					$(this).removeClass "in_error"
 					$("label[for=" + $(this).attr( "id" ) + "]").removeClass "in_error"
-		false
+		if errors.length > 0
+			remove_errors_div( )
+			create_errors_div( )
+			show_error message for message in errors
+			false
+		else
+			true
--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
 (function() {
+  var clear_errors, create_errors_div, remove_errors_div, show_error;
+  create_errors_div = function() {
+    if ($("#form_errors").size() < 1) {
+      return $("<div id=\"form_errors\"><h1>The following errors kept the form from being submitted</h1><ul></ul></div>").prependTo("form");
+    }
+  };
+  remove_errors_div = function() {
+    return $("#form_errors").remove();
+  };
+  show_error = function(message) {
+    return $("<li>" + message + "</li>").appendTo("#form_errors ul");
+  };
+  clear_errors = function() {
+    if ($("#form_errors").size() > 0) {
+      return $("#form_errors ul").empty();
+    }
+  };
   $(document).ready(function() {
     return $("form").submit(function() {
+      var errors, message, _i, _len;
+      errors = [];
       $("form input[type=text]").each(function() {
         if ($(this).attr("value") === "") {
           $(this).addClass("in_error");
           $("label[for=" + $(this).attr("id") + "]").addClass("in_error");
+          errors.push("" + ($("label[for=" + $(this).attr("id") + "]").html()) + " cannot be blank");
           return $(this).focus(function() {
             $(this).removeClass("in_error");
             return $("label[for=" + $(this).attr("id") + "]").removeClass("in_error");
           });
         }
       });
-      return false;
+      if (errors.length > 0) {
+        remove_errors_div();
+        create_errors_div();
+        for (_i = 0, _len = errors.length; _i < _len; _i++) {
+          message = errors[_i];
+          show_error(message);
+        }
+        return false;
+      } else {
+        return true;
+      }
     });
   });
 }).call(this);
